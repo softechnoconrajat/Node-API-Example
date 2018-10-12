05aa93aef806283066b695fdc0c585b051833960
Rajat Kumar Sinha <sinharajatit@hotmail.com>
2018-10-12 13:31:36 +0530

    Add Post /todos route and refractor mongoose

---
+++ server/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+
+var app = express();
+
+
+//configuring the middleware
+
+ app.use(bodyParser.json());
+
+// //creating routes
+// app.post('/todos', (req, res) => {
+//   console.log(req.body);
+//
+//   res.send('doc');
+// });
+
+
+
+
+// creating routes
+
+app.post('/todos', (req, res)=> {
+
+  var todo = new Todo({
+    text : req.body.text
+  });
+
+  todo.save().then((doc)=>{
+    res.send(doc);
+  }, (e)=>{
+    res.status(400).send(e);
+  });
+
+});
+
+app.listen(3000, () => {
+  console.log('Server started at port 3000');
+});
