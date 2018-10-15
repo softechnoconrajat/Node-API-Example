dec5c377502627118d0c2aba2e303b156194dead
Rajat Kumar Sinha <sinharajatit@hotmail.com>
2018-10-15 22:21:04 +0530

    add patch /todos/ids

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
- const express = require('express');
+
+const _ = require('lodash');
+const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
@@ -89,6 +91,37 @@ app.delete('/todos/:id', (req, res) => {
   }).catch((e) => {
     res.status(400).send();
 
+  });
+
+});
+
+
+//to update data. Here we are using npm module patch.
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body , ['text', 'completed']);
+
+  //checking if ObjectId is valid or not.
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  if(_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  }
+  else{
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set:body}, {new:true}).then ((todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
   });
 
 });
