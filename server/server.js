79fb95a13ba6060b3fcaf2e60d353f8c5d724ff0
Rajat Kumar Sinha <sinharajatit@hotmail.com>
2018-10-15 13:37:42 +0530

    Add Fet /dodos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -46,6 +47,29 @@ app.get('/todos', (req, res) => {
   }, (e) => {
         res.status(400).send(e);
   });
+});
+
+// Making query on the basis of id name.
+
+app.get('/todos/:id', (req, res) => {
+
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo)=>{
+
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e)=> {
+      res.status(400).send();
+  });
+
+
 });
 
 app.listen(3000, () => {
