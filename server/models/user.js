7971bb8b0c2023412ee2628d403faa90cc372064
Rajat Kumar Sinha <sinharajatit@hotmail.com>
2018-10-17 01:28:28 +0530

    Add Get /users/me and authentication middle ware

--- server/models/user.js
+++ server/models/user.js
@@ -63,6 +63,33 @@ UserSchema.methods.generateAuthToken = function () {
 
 };
 
+
+
+//definig model method for private authentication.
+ UserSchema.statics.findByToken = function (token) {
+
+   var User = this;
+   var decoded;
+
+   try{
+     decoded = jwt.verify(token, '123abc');
+   }
+   catch (e){
+     // return new Promise((resolve, reject) => {
+     //   reject();
+     // });
+
+     return Promise.reject();
+   }
+
+   return User.findOne({
+     '_id' : decoded._id,
+     'tokens.token' : token,
+     'tokens.access' : 'auth'
+   });
+ };
+
 var User = mongoose.model('User', UserSchema );
+
 
 module.exports =  {User};
--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 3000;
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 
 var app = express();
@@ -141,6 +142,19 @@ app.post('/users', (req, res)=> {
   }).catch((e) => {
     res.status(400).send(e);
   })
+});
+
+
+
+
+//private routing using token VALUE
+
+app.get('/users/me', authenticate, (req, res) => {
+
+
+  res.send(req.user);
+
+
 });
 
 
