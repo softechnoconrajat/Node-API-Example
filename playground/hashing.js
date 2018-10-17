06238b4504977c0d79b9e8febca0bd134389ec14
Rajat Kumar Sinha <sinharajatit@hotmail.com>
2018-10-17 13:30:43 +0530

    Hash userpassword

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,18 +1,34 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc!';
 
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+//
+// });
 
-var token = jwt.sign(data, '123abc');
+var hashPassword = '$2a$10$OrmXu6YB/z4/wjpq.ZS/1uPav/G/PZa2Sf/eb.y5JcUGd9s3nep4i';
 
-console.log(token);
+bcrypt.compare(password, hashPassword, (err, result) => {
+  console.log(result);
+});
 
-var decoded = jwt.verify(token, '123abc');
-
-console.log('Decoded:', decoded);
+// var data = {
+//   id: 10
+// };
+//
+//
+// var token = jwt.sign(data, '123abc');
+//
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc');
+//
+// console.log('Decoded:', decoded);
 
 
 // var text = 'This is the text';
